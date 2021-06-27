2ff9929e289f258cfcf3805c9da768c1fbae40e5
bongoman-by <bongoman@tut.by>
2021-06-27 21:09:45 +0300

    feat(scss): add import to app check 'it work'  #9

--- apps/nx-angular-next/src/app/app.module.ts
+++ apps/nx-angular-next/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, MatCardModule, MatButtonModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
