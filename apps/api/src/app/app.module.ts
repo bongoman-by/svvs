b18dd408bd8e1f19d55493b381dfa77f5cef4f4e
bongoman-by <bongoman@tut.by>
2021-07-04 11:21:28 +0300

    feat(api): add resolver scalar & typeORM setting [WiP] …
    * add resolvers: [resolverMap] => GraphQLModule
    * add entities: [UserEntity] => TypeOrmModule
    * remove providers: [AppResolver] => app.module.ts
    * change app.resolver.ts

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { environment } from '../environments/environment';
-import { GraphQLModule } from '@nestjs/graphql';
-import { AppResolver } from './app.resolver';
+import { resolverMap } from './app.resolver';
+import { UserEntity } from './users/entities/user.entities';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...environment.connection,
+      entities: [UserEntity],
     }),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       context: ({ req }) => ({ req }),
       playground: true,
+      resolvers: [resolverMap],
     }),
   ],
   controllers: [AppController],
-  providers: [AppResolver],
 })
 export class AppModule {}
