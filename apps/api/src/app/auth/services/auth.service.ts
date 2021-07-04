909337dad9b07325f5bbfe238efe3fa5d2567b64
bongoman-by <bongoman@tut.by>
2021-07-04 10:25:21 +0300

    feat(api-auth): add auth module [WIP]
    * change auth.service.ts

--- apps/api/src/app/auth/services/auth.service.ts
+++ apps/api/src/app/auth/services/auth.service.ts
@@ -1,13 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import {
   ISignAuthPayload,
   ISignAuthResponse,
 } from '@svvs/shared/data-access/interfaces';
+import { environment } from '../../../environments/environment';
+import { UserEntity } from '../../users/entities/user.entities';
+import { UserService } from '../../users/services/user.service';
+import { PasswordService } from './password.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+    private readonly passwordService: PasswordService
+  ) {}
+
+  async validateUser(
+    username: string,
+    pass: string
+  ): Promise<Omit<UserEntity, 'password'>> {
+    const user = await this.userService.findOneByUserName(username);
+    const isValid = user
+      ? this.passwordService.compareHash(pass, user.password)
+      : false;
+    if (isValid) {
+      delete user.password;
+      return user;
+    }
+    return null;
+  }
+
   async login(signInPayload: ISignAuthPayload): Promise<ISignAuthResponse> {
-    return await null;
-    // { accessToken, expiresIn, id };
+    const user = await this.validateUser(
+      signInPayload.username,
+      signInPayload.password
+    );
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { username: user.username, userId: user.id };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+      expiresIn: new Date(environment.jwt.expiresIn).getDate(),
+      id: user.id,
+    };
   }
 }
