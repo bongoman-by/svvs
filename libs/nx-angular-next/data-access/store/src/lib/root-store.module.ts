f50096714e43c5da275a2e2235f45984199de475
bongoman-by <bongoman@tut.by>
2021-07-18 16:19:18 +0300

    feat(nx-angular-next): add initial state * serializer [WiP]
    * add      libs/nx-angular-next/data-access/store/src/lib/+state/root-initial-state.ts
    * add      libs/nx-angular-next/data-access/store/src/lib/+state/root-reducer.ts
    *	rename   libs/nx-angular-next/data-access/store/src/lib/interfaces/root-state.interfaces.ts -> libs/nx-angular-next/data-access/store/src/lib/interfaces/root-state.interface.ts
    * change   libs/nx-angular-next/data-access/store/src/lib/root-store.module.ts
    * add      libs/nx-angular-next/data-access/store/src/lib/services/store-router-state-serializer.service.ts
    * change   libs/nx-angular-next/data-access/store/tsconfig.json
      add "strictNullChecks": false,

--- libs/nx-angular-next/data-access/store/src/lib/root-store.module.ts
+++ libs/nx-angular-next/data-access/store/src/lib/root-store.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { rootInitialState } from './+state/root-initial-state';
+import { reducers } from './+state/root-reducer';
+import { StoreRouterStateSerializer } from './services/store-router-state-serializer.service';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    RouterModule,
+    StoreModule.forRoot(reducers, { initialState: rootInitialState }),
+    StoreRouterConnectingModule.forRoot({
+      serializer: StoreRouterStateSerializer,
+    }),
+  ],
 })
-export class RootStoreStoreModule {}
+export class RootStoreModule {}
