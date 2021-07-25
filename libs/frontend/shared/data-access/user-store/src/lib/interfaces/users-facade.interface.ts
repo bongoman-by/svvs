4145ff0cd988a9a642961060ff6afcda9706bca5
bongoman-by <bongoman@tut.by>
2021-07-25 09:43:04 +0300

    fix(user-store): fix user-store [WiP]
    *	modified:   libs/frontend/shared/data-access/user-store/src/lib/interfaces/users-facade.interface.ts
      add
      userLoadRun$
      userLoadFailure$
      abstract loadUser

--- libs/frontend/shared/data-access/user-store/src/lib/interfaces/users-facade.interface.ts
+++ libs/frontend/shared/data-access/user-store/src/lib/interfaces/users-facade.interface.ts
@@ -1,5 +1,6 @@
 import { Observable } from 'rxjs';
-import { IUser } from '@svvs/shared/data-access/interfaces';
+import { IUser } from '@svvs/shared/utils/interfaces';
+import { ApolloError } from '@apollo/client';
 
 /**
  * User facade
@@ -9,4 +10,20 @@ export abstract class IUsersFacade {
    * User entity observer
    */
   user$!: Observable<IUser>;
+
+  /**
+   * User load status observer
+   */
+  userLoadRun$!: Observable<boolean>;
+
+  /**
+   * User load error observer
+   */
+  userLoadFailure$!: Observable<ApolloError>;
+
+  /**
+   * Load user action(dispatch)
+   * @param force Force
+   */
+  abstract loadUser(force?: boolean): void;
 }
