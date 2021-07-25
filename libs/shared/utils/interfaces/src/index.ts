9a5dc9e88e60ec937eb93e5330eace0138b5f7d1
bongoman-by <bongoman@tut.by>
2021-07-25 17:17:33 +0300

    fix(shared-utils-interface): add base cookie [WiP]
    *	modified:   libs/shared/utils/interfaces/src/index.ts
      add
      './lib/storage/cookie-service.interface';
      './lib/storage/cookie-service-option.interface';
    *	new file:   libs/shared/utils/storage/src/lib/services/base-cookie.service.ts
    *	modified:   libs/shared/utils/storage/tsconfig.json
      add
      "strictNullChecks": false
    *	modified:   tsconfig.base.json

--- libs/shared/utils/interfaces/src/index.ts
+++ libs/shared/utils/interfaces/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/auth/sign.auth.interface';
 export * from './lib/apollo/apollo.interface';
 export * from './lib/store/action-force-payload.interface';
 export * from './lib/store/action-effect-payload.interface';
+export * from './lib/storage/cookie-service.interface';
+export * from './lib/storage/cookie-service-option.interface';
