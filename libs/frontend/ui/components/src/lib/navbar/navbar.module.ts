295b9b7728f26202c600fa4fde6c2983bde24dae
bongoman-by <bongoman@tut.by>
2021-08-01 21:11:03 +0300

    feat(ws): add ui libs [WiP]
    * add navbar component

---
+++ libs/frontend/ui/components/src/lib/navbar/navbar.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { navbarContainers } from './navbar.common';
+
+@NgModule({
+  declarations: [...navbarContainers],
+  exports: [...navbarContainers],
+})
+export class NavbarModule {}
